d9b783ab13d7a38efab75fa9ad4342a1fa8c9b2f
TheRolf <roli.vincze@gmail.com>
2016-04-25 21:42:14 +0200

    First version

---
+++ testproject/src/lib/stuff.py
@@ -0,0 +1,14 @@
+'''
+Created on 25 Apr 2016
+
+@author: Rolf
+'''
+
+class HelloWorldClass(object):
+    
+    @staticmethod
+    def helloWorld():
+        print "Hello World!"
+
+if __name__ == '__main__':
+    HelloWorldClass.helloWorld()
